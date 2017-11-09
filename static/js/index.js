981fd9d6e87f5f0b3532710abf5cf17c79de1958
Kerminate <kepeilin@outlook.com>
2017-11-09 15:58:18 +0800

    session

---
+++ static/js/index.js
@@ -0,0 +1,4 @@
+(function () {
+  window.alert('hello koa2 static server')
+  console.log('hello koa2 static server')
+})()
--- util/walk.js
+++ util/walk.js
@@ -8,12 +8,12 @@ const mimes = require('./mimes')
  */
 
 function walk (reqPath) {
-  let files = fs.readFileSync(reqPath)
+  let files = fs.readdirSync(reqPath) // 返回一个包含“指定目录下所有文件名称”的数组对象
   let dirList = []
   let fileList = []
   for (let i = 0, len = files.length; i < len; i++) {
     let item = files[i]
-    let itemArr = item.split('\.')
+    let itemArr = item.split('.')
     let itemMime = (itemArr.length > 1) ? itemArr[itemArr.length - 1] : 'undefined'
     if (typeof mimes[itemMime] === 'undefined') {
       dirList.push(files[i])
